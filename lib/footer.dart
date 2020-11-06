15027e5cc3987ad24939027e4064a1e3be6fb027
yuri sakai <s209qc21t@gmail.com>
2020-11-06 17:02:05 +0900

    first commit.

---
+++ lib/footer.dart
@@ -0,0 +1,85 @@
+import 'package:flutter/material.dart';
+
+class Footer extends StatefulWidget {
+  const Footer();
+
+  @override
+  _Footer createState() => _Footer();
+}
+
+class _Footer extends State<Footer> {
+  int _selectedIndex = 0;
+  final _bottomNavigationBarItems = <BottomNavigationBarItem>[];
+
+  static const _footerIcons = [
+    Icons.home,
+    Icons.textsms,
+    Icons.access_time,
+    Icons.content_paste,
+    Icons.work,
+  ];
+
+  static const _footerItemNames = [
+    'ホーム',
+    'トーク',
+    'タイムライン',
+    'ニュース',
+    'ウォレット',
+  ];
+
+  @override
+  void initState() {
+    super.initState();
+    _bottomNavigationBarItems.add(_UpdateActiveState(0));
+    for (var i = 1; i < _footerItemNames.length; i++) {
+      _bottomNavigationBarItems.add(_UpdateDeactiveState(i));
+    }
+  }
+
+  BottomNavigationBarItem _UpdateActiveState(int index) {
+    return BottomNavigationBarItem(
+        icon: Icon(
+          _footerIcons[index],
+          color: Colors.black87,
+        ),
+        title: Text(
+          _footerItemNames[index],
+          style: TextStyle(
+            color: Colors.black87,
+          ),
+        ));
+  }
+
+  BottomNavigationBarItem _UpdateDeactiveState(int index) {
+    return BottomNavigationBarItem(
+        icon: Icon(
+          _footerIcons[index],
+          color: Colors.black26,
+        ),
+        title: Text(
+          _footerItemNames[index],
+          style: TextStyle(
+            color: Colors.black26,
+          ),
+        ));
+  }
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _bottomNavigationBarItems[_selectedIndex] =
+          _UpdateDeactiveState(_selectedIndex);
+      _bottomNavigationBarItems[index] = _UpdateActiveState(index);
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return BottomNavigationBar(
+      type: BottomNavigationBarType.fixed,
+      items: _bottomNavigationBarItems,
+      currentIndex: _selectedIndex,
+      onTap: _onItemTapped,
+    );
+  }
+}
